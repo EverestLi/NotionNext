bac175fdb54e16f26d56fee6cc0f424121a38369
Xu Li <53181416+EverestLi@users.noreply.github.com>
2026-03-20 21:54:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -52,6 +52,16 @@ const BLOG = {
   CUSTOM_EXTERNAL_JS: [''], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
   CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
+  CUSTOM_CSS: `
+    .header-avatar, .avatar-wrapper, img.avatar {
+        border-radius: 0% !important;
+        width: 320px !important;
+        height: auto !important;
+        border: none !important;
+    }
+    .header-info p { display: none !important; }
+  `,
+
   // 自定义菜单
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu类型的菜单，替代了3.12版本前的Page类型
 
